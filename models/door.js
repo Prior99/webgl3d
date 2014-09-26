8e5338465d2d35f638c7290cf860a40997897623
Prior <fgnodtke@cronosx.de>
2014-09-26 15:04:02 +0200

    Fixed model for door, added new pixelart textures

--- models/door.js
+++ models/door.js
@@ -10,11 +10,11 @@ new Model({
         var p6 = [-.5, -1, -.05];
         var p7 = [ .5, -1, -.05];
 		//Front Back
-        this.quad(p0, p1, p2, p3, [ 0,  0,  1], [0, 0], [1, 2]);
-        this.quad(p4, p5, p6, p7, [ 0,  0, -1], [0, 0], [1, 2]);
+        this.quad(p0, p1, p2, p3, [ 0,  0,  1], [0, 0], [1, 1]);
+        this.quad(p4, p5, p6, p7, [ 0,  0, -1], [0, 0], [1, 1]);
 		//Left and Right
-        this.quad(p0, p2, p4, p6, [-1,  0,  0], [0, 0], [.1, 2]);
-        this.quad(p1, p3, p5, p7, [ 1,  0,  0], [0, 0], [.1, 2]);
+        this.quad(p0, p2, p4, p6, [-1,  0,  0], [0, 0], [.1, 1]);
+        this.quad(p1, p3, p5, p7, [ 1,  0,  0], [0, 0], [.1, 1]);
 		//Top Bottom
         this.quad(p0, p1, p4, p5, [ 0,  1,  0], [0, 0], [1, .1]);
         this.quad(p2, p3, p6, p7, [ 0, -1,  0], [0, .9], [1, 1]);
